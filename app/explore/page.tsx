53f782548b02356d71f794a0f61c6418de9d9ce2
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2024-04-24 16:20:03 -0500

    feat: :sparkles: Add Explore page and redirect from home page

---
+++ app/explore/page.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Explore",
+};
+
+export default function Explore() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <p className="text-gray-500 uppercase text-center">Explore</p>
+        <h1 className="font-serif text-5xl lg:text-8xl tracking-tight text-center">
+          Hypercerts
+        </h1>
+      </div>
+    </main>
+  );
+}
