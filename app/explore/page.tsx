01ff1ef78bf5c129b03f0efb02028c561ee354e3
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2024-04-24 17:31:48 -0500

    feat: :sparkles: Add new hypercert page and form components

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,3 +1,6 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export const metadata = {
   title: "Explore",
 };
@@ -6,10 +9,15 @@ export default function Explore() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <p className="text-gray-500 uppercase text-center">Explore</p>
+        <p className="text-gray-500 uppercase text-center text-sm tracking-wide">
+          Explore
+        </p>
         <h1 className="font-serif text-5xl lg:text-8xl tracking-tight text-center">
           Hypercerts
         </h1>
+        <Link href={"/hypercerts/new"}>
+          <Button variant="default">New Hypercert</Button>
+        </Link>
       </div>
     </main>
   );
---
+++ app/hypercerts/new/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export default function NewHypercert() {
+  return (
+    <main className="flex min-h-screen flex-col justify-betweeen p-8">
+      <h1 className="font-serif text-5xl lg:text-8xl tracking-tight text-center">
+        New hypercert
+      </h1>
+    </main>
+  );
+}
