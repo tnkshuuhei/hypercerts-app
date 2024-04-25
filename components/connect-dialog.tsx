b17d6179fc199f24c64eaf8ba9674c1d35ee1040
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2024-04-25 16:25:22 -0500

    feat(connect-wallet): :iphone: Make custom connect dialog responsive and sort by injected wallets

--- components/connect-dialog.tsx
+++ components/connect-dialog.tsx
@@ -11,6 +11,14 @@ import { useEffect } from "react";
 import { Loader2, Wallet2 } from "lucide-react";
 import Image from "next/image";
 import { DialogTrigger } from "@radix-ui/react-dialog";
+import { useMediaQuery } from "@/hooks/use-media-query";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
 
 export default function ConnectDialog({
   isOpen,
@@ -22,9 +30,18 @@ export default function ConnectDialog({
   const { connect, connectors, error, isPending, variables, reset } =
     useConnect();
   const { isConnected } = useAccount();
+  const isDesktop = useMediaQuery("(min-width: 768px)");
   const walletConnectLogo = "https://avatars.githubusercontent.com/u/37784886";
   const coinbaseWalletSDKLogo =
     "https://avatars.githubusercontent.com/u/1885080";
+
+  console.log("connectors", connectors);
+
+  const customConnectors = [...connectors].sort((a, b) => {
+    if (a.type === "injected" && b.type !== "injected") return -1;
+    if (a.type !== "injected" && b.type === "injected") return 1;
+    return 0;
+  });
 
   useEffect(() => {
     if (isOpen) reset();
@@ -37,7 +54,7 @@ export default function ConnectDialog({
       "id" in variables.connector &&
       connector.id === variables.connector.id
     ) {
-      return <Loader2 className="w-4 h-4" />;
+      return <Loader2 className="w-6 h-6 animate-spin" />;
     }
     return undefined;
   };
@@ -53,50 +70,89 @@ export default function ConnectDialog({
     return connector.icon;
   };
 
-  return (
-    <Dialog open={isOpen} onOpenChange={setIsOpen}>
-      <DialogClose className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground" />
+  const ConnectorContent = () => (
+    <div className="flex flex-col gap-2 pb-5">
+      {customConnectors.map((connector) => (
+        <Button
+          className="flex items-center justify-between gap-2 py-6"
+          disabled={isConnected || isPending}
+          key={connector.id}
+          onClick={() => connect({ connector })}
+          variant="outline"
+        >
+          <div className="flex items-center gap-2">
+            {iconSource(connector) ? (
+              <div className="relative w-6 h-6 object-c object-center rounded-sm overflow-clip">
+                <Image
+                  layout="fill"
+                  src={iconSource(connector) ?? ""}
+                  alt={`${connector.name} icon`}
+                  unoptimized={true}
+                />
+              </div>
+            ) : (
+              <Wallet2 className="w-6 h-6" />
+            )}
+            <p className="font-medium text-base lg:text-lg tracking-tight">
+              {connector.name === "Injected"
+                ? "Browser wallet"
+                : connector.name}
+            </p>
+          </div>
+          {icon(connector)}
+        </Button>
+      ))}
+      {error && (
+        <div className="text-sm p-2 text-center text-red-600 bg-red-100 rounded-md">
+          {error.message}
+        </div>
+      )}
+    </div>
+  );
 
-      <DialogTrigger asChild className={buttonVariants({ variant: "outline" })}>
+  if (isDesktop)
+    return (
+      <Dialog open={isOpen} onOpenChange={setIsOpen}>
+        <DialogClose className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground" />
+
+        <DialogTrigger
+          asChild
+          className={buttonVariants({ variant: "outline" })}
+        >
+          <div className="flex items-center justify-center gap-2">
+            Connect wallet
+            <Wallet2 size={16} />
+          </div>
+        </DialogTrigger>
+
+        <DialogContent>
+          <DialogTitle className="font-serif text-4xl py-3 tracking-tight font-normal">
+            Choose wallet
+          </DialogTitle>
+
+          <ConnectorContent />
+        </DialogContent>
+      </Dialog>
+    );
+
+  return (
+    <Drawer open={isOpen} onOpenChange={setIsOpen}>
+      <DrawerClose className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground" />
+
+      <DrawerTrigger asChild className={buttonVariants({ variant: "outline" })}>
         <div className="flex items-center justify-center gap-2">
           Connect wallet
           <Wallet2 size={16} />
         </div>
-      </DialogTrigger>
+      </DrawerTrigger>
 
-      <DialogContent>
-        <DialogTitle className="font-serif text-3xl">
-          Connect Wallet
-        </DialogTitle>
+      <DrawerContent className="container">
+        <DrawerTitle className="font-serif text-2xl py-3 tracking-normal text-center font-normal">
+          Choose a wallet
+        </DrawerTitle>
 
-        {connectors.map((connector) => (
-          <Button
-            className="justify-between w-52"
-            disabled={isConnected || isPending}
-            key={connector.id}
-            onClick={() => connect({ connector })}
-            variant="outline"
-          >
-            {connector.name}
-            {iconSource(connector) ? (
-              <Image
-                width={16}
-                height={16}
-                src={iconSource(connector) ?? ""}
-                alt={`${connector.name} icon`}
-                unoptimized={true}
-              />
-            ) : (
-              <Wallet2 className="w-4 h-4" />
-            )}
-          </Button>
-        ))}
-        {error && (
-          <div className="p-2 text-center text-red-600 bg-red-200">
-            {error.message}
-          </div>
-        )}
-      </DialogContent>
-    </Dialog>
+        <ConnectorContent />
+      </DrawerContent>
+    </Drawer>
   );
 }
