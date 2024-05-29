34cb7d9f0eb6311ca10befa85709a6dfa2c58c32
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2024-05-29 13:14:18 -0500

    feat(explore): :sparkles: Add search bar input

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -3,9 +3,12 @@
 import HypercertCard, {
   type HypercertCardProps,
 } from "@/components/hypercert-card";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useHypercertClient } from "@/hooks/use-hypercert-client";
 import { validateMetaData } from "@hypercerts-org/sdk";
-import { useEffect, useState } from "react";
+import { Search } from "lucide-react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 
 // export const metadata = {
 //   title: "Explore",
@@ -29,6 +32,11 @@ export default function Explore() {
   const { client } = useHypercertClient();
   const [loading, setLoading] = useState(true);
   const [hypercerts, setHypercerts] = useState<HypercertCardProps[]>([]);
+  const [searchInput, setSearchInput] = useState("");
+
+  const executeSearch = useCallback((searchInput: string) => {
+    console.log(searchInput);
+  }, []);
 
   useEffect(() => {
     const getHypercertMetadata = async (uri: string) => {
@@ -72,9 +80,32 @@ export default function Explore() {
     console.log(hypercerts[0]);
   }
 
+  const SearchBar = () => (
+    <div className="relative flex-1 max-w-xl">
+      <span className="absolute top-1/2 left-2 transform -translate-y-1/2">
+        <Search className="text-slate-00" />
+      </span>
+
+      <div className="flex w-full gap-1">
+        <Input
+          // value={searchInput}
+          className="pl-10 h-10 border-vd-blue-500 bg-vd-beige-100 py-2 text-sm md:text-base font-medium placeholder:text-vd-blue-500/60 ring-offset-white focus-visible:ring-offset-2 focus-visible:ring-vd-blue-400 focus-visible:ring-2"
+          placeholder="Search"
+          // onChange={(e) => setSearchInput(e.target.value)}
+        />
+        <Button
+          className="rounded-md"
+          onClick={() => executeSearch(searchInput)}
+        >
+          Search
+        </Button>
+      </div>
+    </div>
+  );
+
   return (
     <>
-      <main className="flex flex-col p-8 md:p-24 pb-24">
+      <main className="flex flex-col p-8 md:p-24 pb-24 space-y-4">
         <section>
           <h1 className="font-serif text-3xl lg:text-5xl tracking-tight">
             Explore
@@ -86,7 +117,7 @@ export default function Explore() {
           </p>
         </section>
 
-        <div className="p-3"></div>
+        <SearchBar />
         <div className="flex flex-wrap gap-5">
           {hypercerts.map((hypercert) => (
             <HypercertCard {...hypercert} key={hypercert.hypercertId} />
