ab6d1885f9b776f499e7733d4c5f3778ae257bd4
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2024-05-29 13:15:22 -0500

    refactor(explore): :sparkles: Update search UX and deduplicate hypercert response

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -8,7 +8,7 @@ import { Input } from "@/components/ui/input";
 import { useHypercertClient } from "@/hooks/use-hypercert-client";
 import { validateMetaData } from "@hypercerts-org/sdk";
 import { Search } from "lucide-react";
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 // export const metadata = {
 //   title: "Explore",
@@ -28,6 +28,46 @@ type HypercertResponseData = {
   } | null;
 };
 
+const SearchBar = ({
+  searchInput,
+  setSearchInput,
+  executeSearch,
+}: {
+  searchInput: string;
+  setSearchInput: (searchInput: string) => void;
+  executeSearch: (searchInput: string) => void;
+}) => (
+  <div className="relative flex-1 max-w-xl">
+    <span className="absolute top-1/2 left-2 transform -translate-y-1/2">
+      <Search className="text-slate-00" />
+    </span>
+
+    <div className="flex w-full gap-1">
+      <Input
+        value={searchInput}
+        className="pl-10 h-10 border-vd-blue-500 bg-vd-beige-100 py-2 text-sm md:text-base font-medium placeholder:text-vd-blue-500/60 ring-offset-white focus-visible:ring-offset-2 focus-visible:ring-vd-blue-400 focus-visible:ring-2"
+        placeholder="Search"
+        onChange={(e) => setSearchInput(e.target.value)}
+      />
+      <Button className="rounded-md" onClick={() => executeSearch(searchInput)}>
+        Search
+      </Button>
+    </div>
+  </div>
+);
+
+const deDuplicateHypercerts = (hypercerts: HypercertResponseData[]) => {
+  const uniqueHypercerts = [];
+  const seenUris = new Set();
+  for (const hypercert of hypercerts) {
+    if (!seenUris.has(hypercert.uri)) {
+      seenUris.add(hypercert.uri);
+      uniqueHypercerts.push(hypercert);
+    }
+  }
+  return uniqueHypercerts;
+};
+
 export default function Explore() {
   const { client } = useHypercertClient();
   const [loading, setLoading] = useState(true);
@@ -39,26 +79,32 @@ export default function Explore() {
   }, []);
 
   useEffect(() => {
-    const getHypercertMetadata = async (uri: string) => {
+    const getHypercertMetadata = async (uri: string, chainId: number) => {
       const response = await client?.storage.getMetadata(uri);
       const { data, valid, errors } = validateMetaData(response);
       if (valid) {
-        return { hypercertId: uri, ...(data as HypercertCardProps) };
+        return { hypercertId: uri, chainId, ...(data as HypercertCardProps) };
       } else {
         console.log(errors);
       }
       return response;
     };
+
     const getHypercerts = async () => {
       try {
         const response = await client?.indexer.recentHypercerts({ first: 10 });
         const hypercertData = response?.hypercerts
           .data as HypercertResponseData[];
+        console.log(hypercertData);
         if (hypercertData) {
+          const nonDuplicateHypercerts = deDuplicateHypercerts(hypercertData);
           const metadata = await Promise.all(
-            hypercertData.map(async (hypercert) => {
+            nonDuplicateHypercerts.map(async (hypercert) => {
               if (hypercert.uri) {
-                const metadata = await getHypercertMetadata(hypercert.uri);
+                const metadata = await getHypercertMetadata(
+                  hypercert.uri,
+                  hypercert.contract?.chain_id
+                );
                 return metadata;
               }
             })
@@ -80,29 +126,6 @@ export default function Explore() {
     console.log(hypercerts[0]);
   }
 
-  const SearchBar = () => (
-    <div className="relative flex-1 max-w-xl">
-      <span className="absolute top-1/2 left-2 transform -translate-y-1/2">
-        <Search className="text-slate-00" />
-      </span>
-
-      <div className="flex w-full gap-1">
-        <Input
-          // value={searchInput}
-          className="pl-10 h-10 border-vd-blue-500 bg-vd-beige-100 py-2 text-sm md:text-base font-medium placeholder:text-vd-blue-500/60 ring-offset-white focus-visible:ring-offset-2 focus-visible:ring-vd-blue-400 focus-visible:ring-2"
-          placeholder="Search"
-          // onChange={(e) => setSearchInput(e.target.value)}
-        />
-        <Button
-          className="rounded-md"
-          onClick={() => executeSearch(searchInput)}
-        >
-          Search
-        </Button>
-      </div>
-    </div>
-  );
-
   return (
     <>
       <main className="flex flex-col p-8 md:p-24 pb-24 space-y-4">
@@ -117,8 +140,12 @@ export default function Explore() {
           </p>
         </section>
 
-        <SearchBar />
-        <div className="flex flex-wrap gap-5">
+        <SearchBar
+          searchInput={searchInput}
+          setSearchInput={setSearchInput}
+          executeSearch={executeSearch}
+        />
+        <div className="flex justify-center md:justify-start flex-wrap gap-5">
           {hypercerts.map((hypercert) => (
             <HypercertCard {...hypercert} key={hypercert.hypercertId} />
           ))}
