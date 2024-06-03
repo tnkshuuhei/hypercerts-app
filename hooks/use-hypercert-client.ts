38340c1cc56b56bf408e2e4052eb1f9414239a4d
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2024-06-03 11:48:24 -0500

    feat: :recycle: update useHypercertClient hook

--- hooks/use-hypercert-client.ts
+++ hooks/use-hypercert-client.ts
@@ -1,81 +1,27 @@
-"use client";
-import React, { useEffect } from "react";
+import { HypercertClient } from "@hypercerts-org/sdk";
+import { useMemo } from "react";
+import { useChainId, useWalletClient } from "wagmi";
 
-import { HypercertClient, HypercertClientConfig } from "@hypercerts-org/sdk";
-import { base, baseSepolia, celo, optimism, sepolia } from "viem/chains";
-import { useAccount, useWalletClient } from "wagmi";
+export const useHypercertClient = () => {
+  const chainId = useChainId();
+  const { data: walletClient } = useWalletClient();
 
-const isSupportedChain = (chainId: number) => {
-  const supportedChainIds = [
-    base.id,
-    baseSepolia.id,
-    celo.id,
-    optimism.id,
-    sepolia.id,
-  ] as number[];
+  // The SDK will throw an error if the chain is not supported; only testnet for demo purposes
+  const isSupportedChain = (chainId: number) => {
+    return chainId === 11155111 || chainId === 84522;
+  };
 
-  return supportedChainIds.includes(chainId);
-};
-export const useHypercertClient = ({
-  overrideChainId,
-}: {
-  overrideChainId?: number;
-} = {}) => {
-  const { chain } = useAccount();
-  const clientConfig = React.useMemo(
-    () => ({
-      chain: overrideChainId ? { id: overrideChainId } : chain,
-    }),
-    [overrideChainId, chain]
+  const client = useMemo(
+    () =>
+      chainId && isSupportedChain(chainId)
+        ? new HypercertClient({
+            indexerEnvironment: "test",
+            chain: { id: chainId },
+            walletClient: walletClient ?? undefined,
+          })
+        : undefined,
+    [chainId, walletClient]
   );
-  const [client, setClient] = React.useState<HypercertClient | null>(() => {
-    if (clientConfig.chain?.id && isSupportedChain(clientConfig.chain.id)) {
-      return new HypercertClient(clientConfig);
-    }
-    return null;
-  });
-  const [isLoading, setIsLoading] = React.useState(false);
 
-  const {
-    data: walletClient,
-    isError,
-    isLoading: walletClientLoading,
-  } = useWalletClient();
-
-  useEffect(() => {
-    const chainId = overrideChainId || chain?.id;
-    if (
-      chainId &&
-      isSupportedChain(chainId) &&
-      !walletClientLoading &&
-      !isError &&
-      walletClient
-    ) {
-      setIsLoading(true);
-
-      try {
-        const config: Partial<HypercertClientConfig> = {
-          ...clientConfig,
-          chain: { id: chainId },
-          walletClient,
-        };
-
-        const client = new HypercertClient(config);
-        setClient(client);
-      } catch (e) {
-        console.error(e);
-      }
-    }
-
-    setIsLoading(false);
-  }, [
-    chain?.id,
-    clientConfig,
-    isError,
-    overrideChainId,
-    walletClient,
-    walletClientLoading,
-  ]);
-
-  return { client, isLoading };
+  return { client };
 };
