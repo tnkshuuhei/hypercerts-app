40c21abdab5a0774c4475c24df1b6f67dca702c4
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2024-06-03 21:18:13 -0500

    feat(profile): :lipstick: Add empty states and connected address

--- app/profile/[address]/page.tsx
+++ app/profile/[address]/page.tsx
@@ -1,16 +1,40 @@
 "use client";
 
+import { truncateEthereumAddress } from "@/lib/utils";
 import { useAccount } from "wagmi";
+
+const EmptyProfile = () => {
+  return (
+    <div>
+      <p>Connect your wallet to view your profile</p>
+    </div>
+  );
+};
+
+const EmptySection = () => {
+  return (
+    <div className="flex items-center justify-center h-40">
+      <p>Nothing here yet</p>
+    </div>
+  );
+};
 
 const Profile = () => {
   const { address } = useAccount();
 
   if (!address) {
-    return <div>Please connect your wallet to view this page</div>;
+    return <EmptyProfile />;
   }
 
   return (
-    <main className="flex flex-col p-8 md:p-24 pb-24">
+    <section>
+      <div className="flex items-center gap-1 px-2 py-1 bg-slate-100 w-max rounded-lg">
+        <div className="h-2 w-2 bg-green-400 rounded-full"></div>
+        <p className="text-sm">
+          Connected as {truncateEthereumAddress(address)}
+        </p>
+      </div>
+      <div className="p-3" />
       <h1 className="font-serif text-3xl lg:text-5xl tracking-tight">
         Profile
       </h1>
@@ -21,12 +45,7 @@ const Profile = () => {
           </h1>
           <hr className="border-t border-gray-300 my-4" />
         </div>
-        <section className="flex flex-col gap-4">
-          <div className="flex flex-col gap-2">
-            <p>Hypercert</p>
-            <p>Issuer</p>
-          </div>
-        </section>
+        <EmptySection />
       </section>
       <section className="space-y-2">
         <div className="flex flex-col">
@@ -35,14 +54,9 @@ const Profile = () => {
           </h1>
           <hr className="border-t border-gray-300 my-4" />
         </div>
-        <section className="flex flex-col gap-4">
-          <div className="flex flex-col gap-2">
-            <p>Hypercert</p>
-            <p>Issuer</p>
-          </div>
-        </section>
+        <EmptySection />
       </section>
-    </main>
+    </section>
   );
 };
 
