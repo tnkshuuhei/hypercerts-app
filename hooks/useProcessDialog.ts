f629050e8148cd9b2e2ed751c4414ab1080ebe19
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2024-06-05 13:26:57 -0500

    feat: :sparkles: Add useProcessingDialog hook

---
+++ hooks/useProcessDialog.ts
@@ -0,0 +1,55 @@
+import { useState } from "react";
+
+export type StepState = "idle" | "active" | "completed";
+
+export type StepData = {
+  id: string;
+  description: string;
+};
+
+export type DialogStep = {
+  id: StepData["id"];
+  description: StepData["description"];
+  state: StepState;
+};
+
+const useProcessDialog = (steps: StepData[]) => {
+  const createDialogSteps = () => {
+    const dialogSteps: DialogStep[] = steps.map((step) => ({
+      id: step.id,
+      description: step.description,
+      state: "idle",
+    }));
+    return dialogSteps;
+  };
+
+  const [dialogSteps, setDialogSteps] = useState<DialogStep[]>(
+    createDialogSteps()
+  );
+
+  const setStep = (step: DialogStep["id"]) => {
+    const lastStep = dialogSteps[dialogSteps.length - 1];
+    const updatedDialogSteps = dialogSteps.map((dialogStep) => {
+      if (dialogStep.id === step) {
+        return {
+          ...dialogStep,
+          state: dialogStep.id === lastStep.id ? "completed" : "active",
+        };
+      } else if (
+        dialogSteps.indexOf(dialogStep) <
+        dialogSteps.findIndex((ds) => ds.id === step)
+      ) {
+        return { ...dialogStep, state: "completed" };
+      }
+      return dialogStep;
+    });
+    setDialogSteps(updatedDialogSteps as DialogStep[]);
+  };
+
+  return {
+    dialogSteps,
+    setStep,
+  };
+};
+
+export default useProcessDialog;
