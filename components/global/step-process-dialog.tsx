c9a80e5dcdfad26b6faeab5bf6d7f272efbe68c8
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2024-06-05 13:31:57 -0500

    refactor(step-process-dialog): :recycle: Update props and UI content

--- components/global/step-process-dialog.tsx
+++ components/global/step-process-dialog.tsx
@@ -5,85 +5,65 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import {
+  type DialogStep,
+  type StepData,
+  type StepState,
+} from "@/hooks/useProcessDialog";
 import { cn } from "@/lib/utils";
 import { Badge, BadgeCheck, Loader } from "lucide-react";
 import { createElement, useState } from "react";
 
-interface StepState {
-  idle: string;
-  active: string;
-  completed: string;
+interface DialogProps {
+  steps: DialogStep[];
+  title: string;
+  currentStep: StepData["id"];
+  triggerLabel?: string;
+  extraContent?: React.ReactNode;
 }
 
-interface Step {
-  label: string;
-  description?: string;
-  state: keyof StepState;
-}
-
-const steps: Step[] = [
-  {
-    label: "Preparing to mint hypercert",
-    state: "completed",
-  },
-  {
-    label: "Minting hypercert on-chain",
-    state: "active",
-  },
-  {
-    label: "Awaiting confirmation",
-    state: "idle",
-  },
-  {
-    label: "Done minting",
-    state: "idle",
-  },
-];
-
-const stepStateIcons: Record<keyof StepState, React.ElementType> = {
+const stepStateIcons: Record<StepState, React.ElementType> = {
   idle: Badge,
   active: Loader,
   completed: BadgeCheck,
 };
 
-const stateSpecificIconClasses: Record<keyof StepState, string> = {
+const stateSpecificIconClasses: Record<StepState, string> = {
   idle: "text-slate-600",
   active: "text-slate-700 animate-spin bg-white rounded-full",
   completed: "text-slate-400",
 };
 
-const stateSpecificTextClasses: Record<keyof StepState, string> = {
+const stateSpecificTextClasses: Record<StepState, string> = {
   idle: "text-slate-600 font-medium",
   active: "text-slate-700 animate-pulse font-semibold",
   completed: "text-slate-400 font-medium",
 };
 
-const StepProcessModal = () => {
-  const [activeStep, setActiveStep] = useState<Step>(steps[3]);
+const StepProcessModal = ({
+  steps,
+  title,
+  triggerLabel,
+  extraContent,
+  currentStep,
+}: DialogProps) => {
   const lastStep = steps[steps.length - 1];
-  const isLastStepActive = activeStep === lastStep;
   const isLastStepCompleted = lastStep.state === "completed";
-
-  const setNextStep = () => {
-    if (isLastStepActive || isLastStepCompleted) return;
-    steps[steps.indexOf(activeStep!)].state = "completed";
-    steps[steps.indexOf(activeStep!) + 1].state = "active";
-    setActiveStep(steps[steps.indexOf(activeStep!) + 1]);
-  };
+  const [open, setOpen] = useState(true);
 
   return (
-    <Dialog>
-      <DialogTrigger>Mint Hypercert</DialogTrigger>
+    <Dialog open={open} onOpenChange={setOpen}>
+      {triggerLabel && <DialogTrigger>{triggerLabel}</DialogTrigger>}
       <DialogContent>
         <DialogHeader>
           <DialogTitle className="font-serif text-3xl font-normal">
-            Mint hypercert
+            {title}
           </DialogTitle>
         </DialogHeader>
         <div className="flex flex-col px-2 pt-3">
           {steps.map((step) => (
             <div
-              key={step.label}
+              key={step.description}
               className="flex items-center relative border-l-2 border-slate-300 pl-2 pb-6 last-of-type:pb-0"
             >
               <div
@@ -107,13 +87,13 @@ const StepProcessModal = () => {
                     step === lastStep && isLastStepCompleted && "text-green-600"
                   )}
                 >
-                  {step.label}
+                  {step.description}
                 </p>
-                <p className="text-base font-normal">{step.description}</p>
               </div>
             </div>
           ))}
         </div>
+        <div className="flex flex-col px-2 pt-3">{extraContent}</div>
       </DialogContent>
     </Dialog>
   );
