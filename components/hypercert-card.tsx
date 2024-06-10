a4429fe9625151cd354a54685b4c4588920bd1c1
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2024-06-10 15:39:52 -0500

    feat: :sparkles: Add scopes to hypercert card

--- components/hypercert-card.tsx
+++ components/hypercert-card.tsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
-import Link from "next/link";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { Sparkle } from "lucide-react";
+import Image from "next/image";
 import { forwardRef } from "react";
 
 /**
@@ -20,13 +20,13 @@ export interface HypercertCardProps {
   logo?: string;
   fromDateDisplay?: string | null;
   toDateDisplay?: string | null;
-  displayOnly?: boolean;
-  hypercertId?: string;
+  scopes?: string[];
 }
 
 const defaultValues: HypercertCardProps = {
   name: "Your title here",
   description: "Your description here",
+  scopes: [],
 };
 
 const HypercertCard = forwardRef<HTMLDivElement, HypercertCardProps>(
@@ -37,8 +37,8 @@ const HypercertCard = forwardRef<HTMLDivElement, HypercertCardProps>(
       banner,
       fromDateDisplay,
       toDateDisplay,
-      hypercertId,
-      displayOnly = false,
+      logo,
+      scopes,
     }: HypercertCardProps = defaultValues,
     ref
   ) => {
@@ -55,9 +55,9 @@ const HypercertCard = forwardRef<HTMLDivElement, HypercertCardProps>(
     const CardContent = () => (
       <article
         ref={ref}
-        className="relative w-[275px] h-[250px] rounded-xl border-[1.5px] border-slate-500 overflow-clip bg-black"
+        className="relative w-[275px] rounded-xl border-[1.5px] border-slate-500 overflow-clip bg-black"
       >
-        <header className="relative h-[150px] w-full flex items-center justify-center rounded-b-xl overflow-clip">
+        <header className="relative h-[135px] w-full flex items-center justify-center rounded-b-xl overflow-clip">
           {banner ? (
             <Image
               src={banner}
@@ -81,41 +81,51 @@ const HypercertCard = forwardRef<HTMLDivElement, HypercertCardProps>(
             />
           </div>
         </header>
-        {/* <section className="absolute top-16 left-3 border-white border-2 rounded-full overflow-hidden bg-slate-200">
-          <div className="relative w-7 h-7 flex items-center justify-center border border-slate-300 rounded-full overflow-hidden">
+        <section className="absolute top-4 left-3 border-white border-2 rounded-full overflow-hidden bg-slate-200">
+          <div className="relative w-8 h-8 flex items-center justify-center border border-slate-300 rounded-full overflow-hidden">
             {logo ? (
-              <Image src={logo} alt={`${title} logo`} fill unoptimized />
+              <Image
+                src={logo}
+                alt={`${title} logo`}
+                fill
+                unoptimized
+                className="object-cover"
+              />
             ) : (
               <div className="flex items-center justify-center bg-slate-300 h-10 w-10">
                 <Sparkle size={24} />
               </div>
             )}
           </div>
-        </section> */}
-        <section className="p-3 rounded-t-xl h-full bg-white border-t-[1.5px] border-black space-y-2">
+        </section>
+        <section className="p-3 pt-4 rounded-t-xl bg-white border-t-[1.5px] border-black space-y-2">
+          <div className="flex items-center">
+            <span className="text-xs text-slate-600 uppercase">
+              {formattedDateRange}
+            </span>
+          </div>
           <h5
-            className="text-base font-semibold text-slate-800 line-clamp-2 text-ellipsis tracking-tight leading-tight"
+            className="text-base font-semibold text-slate-800 h-10 line-clamp-2 text-ellipsis tracking-tight leading-tight"
             title={title}
           >
             {title}
           </h5>
-          <div className="flex items-center">
-            <span className="text-sm text-slate-500">{formattedDateRange}</span>
-          </div>
+          <ScrollArea className="h-[50px]">
+            <div className="flex flex-wrap gap-1">
+              {scopes?.map((scope) => (
+                <span
+                  key={scope}
+                  className="text-xs text-slate-600 px-1 py-0.5 border-[1.5px] border-slate-300 rounded-lg"
+                >
+                  {scope}
+                </span>
+              ))}
+            </div>
+          </ScrollArea>
         </section>
       </article>
     );
-    return displayOnly ? (
-      <CardContent />
-    ) : (
-      <Link
-        href={hypercertId ? `/hypercert/${hypercertId}` : "#"}
-        passHref
-        className="w-max hover:-translate-y-1 transition-transform duration-200 ease-[cubic-bezier(.44,.95,.63,.96)]"
-      >
-        <CardContent />
-      </Link>
-    );
+    return <CardContent />;
   }
 );
 
