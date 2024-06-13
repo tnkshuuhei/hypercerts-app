7c40cc34d652303277c02550e570753b4ae0da98
Kristofer <kristofer@fmckl.se>
2024-06-13 14:35:09 +0200

    Create evaluation, WIP

--- components/hypercert/evaluate-button.tsx
+++ components/hypercert/evaluate-button.tsx
@@ -9,6 +9,7 @@ import {
 import { useEffect, useState } from "react";
 
 import { Button } from "@/components/ui/button";
+import { Drawer } from "vaul";
 import { TrustedAttestor } from "../../github/types/trusted-attestor.type";
 import { useAccount } from "wagmi";
 
@@ -29,7 +30,38 @@ export default function EvaluateButton() {
   const enabled = address && evaluator;
 
   if (enabled) {
-    return <Button>Evaluate this Hypercert</Button>;
+    return (
+      <Drawer.Root direction="right">
+        <Drawer.Trigger asChild>
+          <div>
+            <Button>Evaluate this Hypercert</Button>
+          </div>
+        </Drawer.Trigger>
+        <Drawer.Portal>
+          <Drawer.Overlay className="fixed inset-0 bg-black/40" />
+          <Drawer.Content className="bg-white flex flex-col rounded-t-[10px] h-full w-[500px] mt-24 fixed bottom-0 right-0">
+            <div className="p-4 bg-white flex-1 h-full">
+              <div className="max-w-md mx-auto flex flex-col gap-5">
+                <Drawer.Title className="font-serif text-3xl lg:text-5xl tracking-tight">
+                  Evaluate Hypercert
+                </Drawer.Title>
+                <p>
+                  Review and evaluate the information in the Hypercert. Create
+                  attestations where you mark sections as <strong>valid</strong>{" "}
+                  or <strong>invalid</strong>. You also have the option to leave
+                  a comment that will be displayed along with your evaluation.
+                </p>
+                <p>
+                  All attestation fields are optional. Review only those
+                  sections where you confidently can attest to the correctness
+                  of the data.
+                </p>
+              </div>
+            </div>
+          </Drawer.Content>
+        </Drawer.Portal>
+      </Drawer.Root>
+    );
   }
 
   return (
