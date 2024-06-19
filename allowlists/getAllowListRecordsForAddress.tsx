07dd4256942ec744d28015717c996b1b73f2889c
Kristofer <kristofer@fmckl.se>
2024-06-19 16:13:52 +0200

    Create an AllowListRecordFragment to more easily pass information around

--- allowlists/getAllowListRecordsForAddress.tsx
+++ allowlists/getAllowListRecordsForAddress.tsx
@@ -1,27 +1,39 @@
 import "server-only";
 
+import { ResultOf, graphql } from "@/lib/graphql";
+
 import { HYPERCERTS_API_URL } from "../configs/hypercerts";
-import { graphql } from "@/lib/graphql";
 import request from "graphql-request";
 
-const query = graphql(`
-  query allowlistRecords($address: String) {
-    allowlistRecords(where: { user_address: { contains: $address } }) {
-      count
-      data {
-        hypercert_id
-        token_id
-        leaf
-        entry
-        user_address
-        claimed
-        proof
-        units
-        total_units
-      }
-    }
+export const AllowListRecordFragment = graphql(`
+  fragment AllowListRecordFragment on AllowlistRecord {
+    id
+    hypercert_id
+    token_id
+    leaf
+    entry
+    user_address
+    claimed
+    proof
+    units
+    total_units
   }
 `);
+export type AllowListRecord = ResultOf<typeof AllowListRecordFragment>;
+
+const query = graphql(
+  `
+    query allowlistRecords($address: String) {
+      allowlistRecords(where: { user_address: { contains: $address } }) {
+        count
+        data {
+          ...AllowListRecordFragment
+        }
+      }
+    }
+  `,
+  [AllowListRecordFragment],
+);
 
 export async function getAllowListRecordsForAddress(address: string) {
   const res = await request(HYPERCERTS_API_URL, query, {
