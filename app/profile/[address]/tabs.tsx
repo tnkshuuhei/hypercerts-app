d7c20ff74e39a29938ef1f43c066abddc6fdf3f7
Jip Stavenuiter <jipstavenuiter@gmail.com>
2024-06-23 07:33:42 -0400

    Feature/hyperboard grid (#66)
    * (feat): create grid for hyperboards
    
    * (feat): add collections detail page

--- app/profile/[address]/tabs.tsx
+++ app/profile/[address]/tabs.tsx
@@ -7,13 +7,16 @@ import { getHypercertsByCreator } from "@/hypercerts/getHypercertsByCreator";
 import { type SupportedChainIdType } from "@/lib/constants";
 import { supabaseData } from "@/lib/supabase";
 import Link from "next/link";
-import Script from "next/script";
 import { Suspense } from "react";
+import { HyperboardRow } from "@/components/hyperboard/hyperboard-row";
 
-const HyperBoardsTabContentInner = async ({ address }: { address: string }) => {
+export const defaultDescription =
+  "libp2p is an open source project for building network applications free from runtime and address services. You can help define the specification, create applications using libp2p, and craft examples and tutorials to get involved.";
+
+const CollectionsTabContentInner = async ({ address }: { address: string }) => {
   const hyperboards = await supabaseData
     .from("hyperboards")
-    .select("id")
+    .select("id, name")
     .eq("admin_id", address.toLowerCase());
 
   if (!hyperboards || !hyperboards.data) {
@@ -22,27 +25,24 @@ const HyperBoardsTabContentInner = async ({ address }: { address: string }) => {
 
   return (
     <div>
-      <Script
-        src="https://hyperboards-git-feature-hyperboard-widget-hypercerts-foundation.vercel.app/widget/hyperboard-widget.js"
-        type="module"
-      />
       <div className="flex flex-col gap-4">
         {hyperboards.data.map((hyperboard) => (
-          <div
+          <HyperboardRow
             key={hyperboard.id}
-            className="hyperboard-widget"
-            data-hyperboard-id={hyperboard.id}
-          ></div>
+            hyperboardId={hyperboard.id}
+            name={hyperboard.name}
+            description={defaultDescription}
+          />
         ))}
       </div>
     </div>
   );
 };
 
-const HyperboardsTabContent = ({ address }: { address: string }) => {
+const CollectionsTabContent = ({ address }: { address: string }) => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <HyperBoardsTabContentInner address={address} />
+      <CollectionsTabContentInner address={address} />
     </Suspense>
   );
 };
@@ -114,13 +114,13 @@ const ProfileTabSection = ({
             <h3 className={`text-lg`}>Hypercerts</h3>
           </Button>
         </Link>
-        <Link href={`/profile/${address}?tab=hyperboards`}>
+        <Link href={`/profile/${address}?tab=collections`}>
           <Button
-            variant={active === "hyperboards" ? "default" : "outline"}
+            variant={active === "collections" ? "default" : "outline"}
             size={"default"}
             className={`space-x-1 border-[1.5px]`}
           >
-            <h3 className={`text-lg`}>Hyperboards</h3>
+            <h3 className={`text-lg`}>Collections</h3>
           </Button>
         </Link>
       </section>
@@ -128,4 +128,4 @@ const ProfileTabSection = ({
   );
 };
 
-export { HyperboardsTabContent, HypercertsTabContent, ProfileTabSection };
+export { CollectionsTabContent, HypercertsTabContent, ProfileTabSection };
