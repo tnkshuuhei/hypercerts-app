575769a26f87d22cab7209b5f866bcb42b84290f
bitbeckers <code@bitbeckers.com>
2024-08-04 18:59:31 +0200

    fix(load): bypass hypercerts.orders.null error

--- hypercerts/getHypercertsByCreator.ts
+++ hypercerts/getHypercertsByCreator.ts
@@ -1,49 +1,58 @@
 import "server-only";
 
-import { graphql, readFragment } from "@/lib/graphql";
+import {graphql, readFragment} from "@/lib/graphql";
 
-import { HYPERCERTS_API_URL } from "@/configs/hypercerts";
-import { HypercertListFragment } from "@/hypercerts/fragments/hypercert-list.fragment";
+import {HYPERCERTS_API_URL} from "@/configs/hypercerts";
+import {HypercertListFragment} from "@/hypercerts/fragments/hypercert-list.fragment";
 import request from "graphql-request";
 
 const query = graphql(
-  `
-    query AllHypercerts($where: HypercertsWhereInput) {
-      hypercerts(where: $where, count: COUNT) {
-        count
-        data {
-          ...HypercertListFragment
+        `
+        query AllHypercerts($where: HypercertsWhereInput) {
+            hypercerts(where: $where, count: COUNT) {
+                count
+                data {
+                    ...HypercertListFragment
+                }
+            }
         }
-      }
-    }
-  `,
-  [HypercertListFragment],
+    `,
+    [HypercertListFragment],
 );
 
 export async function getHypercertsByCreator({
-  creatorAddress,
-}: {
-  creatorAddress: string;
+                                                 creatorAddress,
+                                             }: {
+    creatorAddress: string;
 }) {
-  const res = await request(HYPERCERTS_API_URL, query, {
-    where: {
-      creator_address: {
-        contains: creatorAddress,
-      },
-    },
-  });
+    const res = {
+        count: null,
+        data: null
+    }
 
-  // TODO: Throw error?
-  if (!res.hypercerts?.data) {
-    return undefined;
-  }
+    try {
+        const queryRes = await request(HYPERCERTS_API_URL, query, {
+            where: {
+                creator_address: {
+                    contains: creatorAddress,
+                },
+            },
+        });
 
-  const processedFragments = res.hypercerts.data.map((hypercert) => {
-    return readFragment(HypercertListFragment, hypercert);
-  });
+        // TODO: Throw error?
+        if (!queryRes.hypercerts?.data) {
+            return undefined;
+        }
 
-  return {
-    count: res.hypercerts.count,
-    data: processedFragments,
-  };
+        res.count = queryRes.hypercerts.count;
+        res.data = queryRes.hypercerts.data.map((hypercert) => {
+            return readFragment(HypercertListFragment, hypercert);
+        })
+
+        return res;
+    } catch (e) {
+        console.error(`[getHypercertsByCreator] Error fetching hypercerts by creator: ${(e as Error).message}`);
+        return undefined;
+    }
+
 }
