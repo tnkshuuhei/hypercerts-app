e8b5e0c296eb34c367f9637f87305770dbe95d4e
bitbeckers <code@bitbeckers.com>
2024-09-03 14:01:19 +0200

    feat(icons): x-spacing

--- components/global/footer.tsx
+++ components/global/footer.tsx
@@ -14,16 +14,48 @@ const footerLinks = [
   },
 ];
 
+const footerIcons = [
+  {
+    label: "Twitter",
+    url: "https://x.com/hypercerts",
+    icon: "/socials_icons/twitter-logo.png",
+  },
+  {
+    label: "Discord",
+    url: "https://discord.gg/VVSyKg75",
+    icon: "/socials_icons/discord-logo.png",
+  },
+  {
+    label: "Hypercerts Foundation",
+    url: "/",
+    icon: "/hypercerts-logo.png",
+  },
+  {
+    label: "Github",
+    url: "https://github.com/hypercerts-org/",
+    icon: "/socials_icons/github-logo.png",
+  },
+  {
+    label: "Telegram",
+    url: "https://hypercerts.org/docs/",
+    icon: "/socials_icons/telegram-logo.png",
+  },
+];
+
 const Footer = () => (
   <footer className="flex flex-col space-y-2 justify-center items-center py-5 px-3 border-t-[1.5px] border-black pb-24 md:pb-5 bg-slate-50 mt-auto">
-    <Link className="relative" href={"/"} title="Hypercerts">
-      <Image
-        src={"/hypercerts-logo.png"}
-        alt="Hypercerts logo"
-        width={48}
-        height={48}
-      />
-    </Link>
+    <ul className="flex space-x-4">
+      {footerIcons.map((item) => (
+        <Link
+          className="relative"
+          href={item.url}
+          title={item.label}
+          key={item.label}
+        >
+          <Image src={item.icon} alt={item.label} width={48} height={48} />
+        </Link>
+      ))}
+    </ul>
 
     <ul className="flex space-x-1">
       {footerLinks.map((item) => (
@@ -34,7 +66,8 @@ const Footer = () => (
     </ul>
 
     <p className="flex space-x-1 items-center text-sm">
-      <span>Copyright</span> <Copyright size={12} /> <span>{new Date().getFullYear()}</span>
+      <span>Copyright</span> <Copyright size={12} />{" "}
+      <span>{new Date().getFullYear()}</span>
       <span>Hypercerts Foundation</span>
     </p>
   </footer>
