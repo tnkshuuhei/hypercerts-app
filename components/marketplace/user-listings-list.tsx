68281dadcd9c8b1f3b274f04361cb2303c2656c6
Jip Stavenuiter <jipstavenuiter@gmail.com>
2024-09-03 12:15:19 +0200

    fix: no listings and no collections messages (#216)

--- components/marketplace/user-listings-list.tsx
+++ components/marketplace/user-listings-list.tsx
@@ -344,7 +344,7 @@ export default function UserListingsList({
   };
 
   if (!orders?.length) {
-    return <div>This Hypercert has not yet been listed for sale.</div>;
+    return <div>No listings yet.</div>;
   }
 
   const classes = (onConnectedChain: boolean) =>
