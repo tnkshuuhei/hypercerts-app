28789ce3db7e0f89c5f9505fdbf6a020dea7872a
jipstavenuiter <jipstavenuiter@gmail.com>
2024-09-25 15:48:02 +0200

    chore: possible bug fix?

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,6 +1,8 @@
+"use client";
+
 import { SettingsForm } from "@/components/settings/settings-form";
 
-export default async function SettingsPage() {
+export default function SettingsPage() {
   return (
     <main className="flex flex-col p-8 md:px-24 pt-8 pb-24 space-y-4 flex-1 container max-w-screen-md">
       <section className="flex flex-col gap-2">
