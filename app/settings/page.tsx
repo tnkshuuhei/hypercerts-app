3e502bef742d6325ad96a9b2c6af7b73bb3afb2b
Jip Stavenuiter <jipstavenuiter@gmail.com>
2024-09-25 14:47:17 +0200

    Feature/settings page (#251)
    * feat: add user settings form
    
    * feat: get user info from backend and prefill settings form
    
    * feat: use sign typed data
    
    * feat: use sign typed data with domain
    
    * feat: rename request to UpdateUserRequest

---
+++ app/settings/page.tsx
@@ -0,0 +1,20 @@
+import { SettingsForm } from "@/components/settings/settings-form";
+
+export default async function SettingsPage() {
+  return (
+    <main className="flex flex-col p-8 md:px-24 pt-8 pb-24 space-y-4 flex-1 container max-w-screen-md">
+      <section className="flex flex-col gap-2">
+        <h1 className="font-serif text-3xl lg:text-5xl tracking-tight">
+          Settings
+        </h1>
+        <h4 className="mb-3 opacity-50">
+          Your display name and image will appear on hyperboards that include
+          hypercerts that you own.
+        </h4>
+      </section>
+      <section className="flex flex-col gap-2">
+        <SettingsForm />
+      </section>
+    </main>
+  );
+}
