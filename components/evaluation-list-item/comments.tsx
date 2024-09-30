83b110d710db8c0a5f44e65ed96466c5e804e845
bitbeckers <code@bitbeckers.com>
2024-09-30 14:39:15 +0200

    chore: code cleanup

--- components/evaluation-list-item/comments.tsx
+++ components/evaluation-list-item/comments.tsx
@@ -11,16 +11,7 @@ export default function Comments({ comments }: CommentsProps) {
   const [showFullComments, setShowFullComments] = useState(false);
   if (!comments) return null;
 
-  // Truncate the comments to a maximum of 200 characters
-  // If the comments are longer than 200 characters, display the first 200 characters and add an ellipsis
-  // If the comments are shorter than 200 characters, display the full comments
-  // When the user clicks on 'Show full comments', display the full comments and replace the button with 'Show less'
-
-  // // generate random text for testing purposes
-  const text =
-    "Fuga qui expedita sit labore repellendus esse. Dolorem quisquam voluptates error provident. Saepe quisquam atque consequatur repudiandae cupiditate. Et cupiditate rem laboriosam eveniet ut praesentium aliquam. Et recusandae possimus excepturi est dolores laudantium sed. Tempora voluptatem quae fuga assumenda. Repudiandae commodi eius sapiente optio ducimus. In laboriosam rerum aut nam suscipit repudiandae et. Ab ut sequi inventore soluta eum suscipit molestiae unde. Sit et est modi. Totam dolores culpa consequuntur id esse impedit quo omnis.";
-
-  const truncatedComments = truncate(text, {
+  const truncatedComments = truncate(comments, {
     length: 200,
     separator: "...",
   });
@@ -45,7 +36,7 @@ export default function Comments({ comments }: CommentsProps) {
     <div className="flex flex-col gap-2">
       <div className="flex flex-col gap-2">
         <div className="overflow-hidden text-sm text-slate-700">
-          {showFullComments ? text : truncatedComments}
+          {showFullComments ? comments : truncatedComments}
         </div>
       </div>
       <div>
