265a3b7a68335aee6d1734dee99fad3d85bc6783
shuhei tanaka <shuhei46491@gmail.com>
2024-10-13 15:11:03 +0700

    fix: generateBlockExplorerLink func

--- lib/utils.ts
+++ lib/utils.ts
@@ -51,7 +51,28 @@ export const generateBlockExplorerLink = (
   if (!chain) {
     return "";
   }
-  return `https://${chain?.id === 1 ? "" : `${chain?.name}.`}etherscan.io/tx/${transactionHash}`;
+  return `${getBlockExplorerPath(chain)}/tx/${transactionHash}`;
+};
+
+export const getBlockExplorerPath = (chain: Chain | undefined) => {
+  if (!chain) {
+    return "";
+  }
+  // by default, we use the default block explorer
+  switch (chain.id) {
+    case 1: // Ethereum Mainnet
+      return "https://etherscan.io";
+    case 10: // Optimism Mainnet
+      return "https://optimistic.etherscan.io";
+    case 8453: // Base Mainnet
+      return "https://basescan.org";
+    case 42220: // Celo Mainnet
+      return "https://celoscan.io";
+    case 42161: // Arbitrum Mainnet
+      return "https://arbiscan.io";
+    default:
+      return `${chain.blockExplorers?.default.url}`;
+  }
 };
 
 export const containsMarkdown = (text: string): boolean => {
