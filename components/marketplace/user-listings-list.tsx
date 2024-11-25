048b5b05a25bf21e422885e6f9d7c0899094ffed
bitbeckers <code@bitbeckers.com>
2024-11-25 14:51:17 +0900

    fix(marketplace): remove beta banner

--- components/marketplace/user-listings-list.tsx
+++ components/marketplace/user-listings-list.tsx
@@ -475,10 +475,6 @@ export default function UserListingsList({
         >
           <DialogContent className="gap-5 max-w-2xl">
             <DialogHeader>
-              <div className="bg-yellow-500/10 p-3 mb-2 rounded-sm">
-                Hypercerts marketplace features are in beta. Please use with
-                caution.
-              </div>
               <DialogTitle className="font-serif text-3xl font-medium tracking-tight">
                 Buy hypercert fraction
               </DialogTitle>
