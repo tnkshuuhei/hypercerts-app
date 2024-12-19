6ce6d12285285d34c6a48f097ec4d55200db170b
shuhei tanaka <shuhei46491@gmail.com>
2024-12-19 16:09:51 +0900

    chore: alert external source

--- components/creator-feed/source.tsx
+++ components/creator-feed/source.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 import { Button } from "../ui/button";
 import { ArrowUpRight, File, LinkIcon } from "lucide-react";
-
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import Link from "next/link";
 interface SourceProps {
   type: string;
   src: string;
@@ -11,16 +21,41 @@ interface SourceProps {
 export default function Source({ type, src }: SourceProps) {
   // TODO: implement dialog
   return (
-    <Button variant={"link"} className="cursor-pointer text-sm p-0">
-      <div className="flex flex-row items-center">
-        {type === "url" ? (
-          <LinkIcon className="h-4 w-4 mr-2" />
-        ) : (
-          <File className="h-4 w-4 mr-2" />
-        )}
-        <span className="truncate flex-1">{src}</span>
-        <ArrowUpRight className="h-4 w-4" />
-      </div>
-    </Button>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant={"link"} className="cursor-pointer text-sm p-0">
+          <div className="flex flex-row items-center">
+            {type === "url" ? (
+              <LinkIcon className="h-4 w-4 mr-2" />
+            ) : (
+              <File className="h-4 w-4 mr-2" />
+            )}
+            <span className="truncate flex-1">{src}</span>
+          </div>
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            You are about to view external content. Please continue with
+            caution.
+          </AlertDialogTitle>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel className="w-1/2">Go back</AlertDialogCancel>
+          <Link
+            href={src}
+            className="w-1/2"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <AlertDialogAction className="w-full">
+              Open link
+              <ArrowUpRight className="h-4 w-4 mx-2" />
+            </AlertDialogAction>
+          </Link>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 }
