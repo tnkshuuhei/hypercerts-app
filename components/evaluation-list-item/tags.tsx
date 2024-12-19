5488a7d444a37be2910c0d944751bf7db7834975
bitbeckers <code@bitbeckers.com>
2024-12-19 12:32:35 +0100

    style(tags.tsx): cleanup rendered tags
    more compact rendering

--- components/evaluation-list-item/tags.tsx
+++ components/evaluation-list-item/tags.tsx
@@ -10,14 +10,12 @@ export default function Tags({
 }) {
   if (!tags || tags.length === 0) return null;
   return (
-    <div className="flex flex-col items-start w-full" {...props}>
-      <div className="flex items-start w-full gap-1" {...props}>
-        {tags.map((tag, index) => (
-          <Badge variant="secondary" key={index}>
-            {tag}
-          </Badge>
-        ))}
-      </div>
+    <div className="flex flex-wrap items-start w-full gap-1" {...props}>
+      {tags.map((tag, index) => (
+        <Badge variant="secondary" key={index} className="text-xs py-0 px-2">
+          {tag}
+        </Badge>
+      ))}
     </div>
   );
 }
