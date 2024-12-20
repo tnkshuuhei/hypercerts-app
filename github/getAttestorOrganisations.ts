599f8069db136673117f25b86e2ef381b8866449
bitbeckers <code@bitbeckers.com>
2024-12-20 12:16:28 +0100

    fix(attestors): updates trusted attestor github paths

--- github/getAttestorOrganisations.ts
+++ github/getAttestorOrganisations.ts
@@ -4,7 +4,7 @@ export async function getAttestorOrganisations(): Promise<
   AttestorOrganisation[]
 > {
   const res = await fetch(
-    "https://github.com/hypercerts-org/hypercerts-attestor-registry/raw/main/org.json"
+    "https://raw.githubusercontent.com/hypercerts-org/hypercerts-attestor-registry/refs/heads/main/org.json",
   );
   return res.json();
 }
