05182372b302861346afcb1a5a8956d64001ddf8
shuhei tanaka <shuhei46491@gmail.com>
2025-02-04 17:56:49 +0900

    feat(profile): enhance unclaimed hypercert claim flow with step-by-step dialog

--- components/profile/unclaimed-hypercert-claim-button.tsx
+++ components/profile/unclaimed-hypercert-claim-button.tsx
@@ -7,6 +7,10 @@ import { waitForTransactionReceipt } from "viem/actions";
 import { useAccount, useWalletClient } from "wagmi";
 import { useRouter } from "next/navigation";
 import { Row } from "@tanstack/react-table";
+import { useStepProcessDialogContext } from "../global/step-process-dialog";
+import { createExtraContent } from "../global/extra-content";
+import { revalidatePathServerAction } from "@/app/actions/revalidatePathServerAction";
+import { useState } from "react";
 
 interface UnclaimedHypercertClaimButtonProps {
   allowListRecord: Row<AllowListRecord>;
@@ -17,10 +21,24 @@ export default function UnclaimedHypercertClaimButton({
 }: UnclaimedHypercertClaimButtonProps) {
   const { client } = useHypercertClient();
   const { data: walletClient } = useWalletClient();
-  const { address } = useAccount();
+  const account = useAccount();
   const { refresh } = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+  const { setDialogStep, setSteps, setOpen, setTitle, setExtraContent } =
+    useStepProcessDialogContext();
 
   const claimHypercert = async () => {
+    setIsLoading(true);
+    setOpen(true);
+    setSteps([
+      { id: "preparing", description: "Preparing to claim hypercert..." },
+      { id: "claiming", description: "Claiming hypercert on-chain..." },
+      { id: "confirming", description: "Waiting for on-chain confirmation" },
+      { id: "route", description: "Creating your new hypercert's link..." },
+      { id: "done", description: "Claiming complete!" },
+    ]);
+
+    setTitle("Claim Hypercert from Allowlist");
     if (!client) {
       throw new Error("No client found");
     }
@@ -29,7 +47,7 @@ export default function UnclaimedHypercertClaimButton({
       throw new Error("No wallet client found");
     }
 
-    if (!address) {
+    if (!account) {
       throw new Error("No address found");
     }
 
@@ -40,34 +58,66 @@ export default function UnclaimedHypercertClaimButton({
     ) {
       throw new Error("Invalid allow list record");
     }
+    await setDialogStep("preparing, active");
 
     console.log(allowListRecord);
 
-    const tx = await client.mintClaimFractionFromAllowlist(
-      BigInt(allowListRecord.original?.token_id),
-      BigInt(allowListRecord.original?.units),
-      allowListRecord.original?.proof as `0x${string}`[],
-      undefined,
-    );
-    console.log(tx);
-    if (!tx) {
-      throw new Error("Failed to claim hypercert");
+    try {
+      await setDialogStep("claiming", "active");
+      const tx = await client.mintClaimFractionFromAllowlist(
+        BigInt(allowListRecord.original?.token_id),
+        BigInt(allowListRecord.original?.units),
+        allowListRecord.original?.proof as `0x${string}`[],
+        undefined,
+      );
+      console.log(tx);
+
+      if (!tx) {
+        await setDialogStep("claiming", "error");
+        throw new Error("Failed to claim hypercert");
+      }
+
+      await setDialogStep("confirming", "active");
+      const receipt = await waitForTransactionReceipt(walletClient, {
+        confirmations: 3,
+        hash: tx,
+      });
+
+      if (receipt.status == "success") {
+        await setDialogStep("route", "active");
+        const extraContent = createExtraContent(
+          receipt,
+          allowListRecord.original?.hypercert_id!,
+          account.chain,
+        );
+        setExtraContent(extraContent);
+        await setDialogStep("done", "completed");
+        await revalidatePathServerAction([
+          `/hypercerts/${allowListRecord.original?.hypercert_id}`,
+          `/profile/${account.address}`,
+          `/profile/${account.address}?tab=hypercerts-claimable`,
+        ]);
+      } else if (receipt.status == "reverted") {
+        await setDialogStep("confirming", "error", "Transaction reverted");
+      }
+      console.log({ receipt });
+      setTimeout(() => {
+        refresh();
+      }, 5000);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
     }
-
-    await waitForTransactionReceipt(walletClient, {
-      hash: tx,
-    });
-
-    setTimeout(() => {
-      refresh();
-    }, 5000);
   };
   return (
     <Button
       variant={"outline"}
       size={"sm"}
       onClick={claimHypercert}
-      disabled={allowListRecord.original?.user_address !== address}
+      disabled={
+        allowListRecord.original?.user_address !== account.address || isLoading
+      }
     >
       Claim
     </Button>
