48e45ad8e5176a56583fca8f007dd76594518f34
shuhei tanaka <shuhei46491@gmail.com>
2025-02-18 17:31:23 +0900

    feat(dialog): add toast for step process dialog
    This change is intended to allow the user to leave the step dialog during the tx execution(any kind of action) and to check  the status on toast. toast is displayed when user closes the dialog

--- components/global/step-process-dialog.tsx
+++ components/global/step-process-dialog.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { buttonVariants } from "@/components/ui/button";
+
+import { Button, buttonVariants } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
@@ -10,7 +11,14 @@ import {
 } from "@/components/ui/dialog";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { cn } from "@/lib/utils";
-import { AlertCircle, Badge, BadgeCheck, Loader } from "lucide-react";
+import {
+  AlertCircle,
+  Badge,
+  BadgeCheck,
+  Loader,
+  CheckCircle2,
+  XCircle,
+} from "lucide-react";
 import React, {
   createContext,
   createElement,
@@ -20,6 +28,8 @@ import React, {
   useEffect,
   useState,
 } from "react";
+import { useToast } from "@/components/ui/use-toast";
+import { ToastAction } from "@/components/ui/toast";
 
 export type StepState = "idle" | "active" | "completed" | "error";
 
@@ -32,7 +42,77 @@ export type DialogStep = {
 
 export type StepData = Pick<DialogStep, "id" | "description">;
 
+const ToastStepper = () => {
+  const { toast } = useToast();
+  const { dialogSteps, setOpen, open } = useStepProcessDialogContext();
+
+  useEffect(() => {
+    if (open) return;
+
+    const activeStep = dialogSteps.find((step) => step.state === "active");
+    const errorStep = dialogSteps.find((step) => step.state === "error");
+    const lastStep = dialogSteps[dialogSteps.length - 1];
+    const isComplete = lastStep?.state === "completed";
+
+    if (activeStep) {
+      // Show toast while action is in progress
+      toast({
+        duration: Infinity, // Keep toast visible until completes
+        description: (
+          <div className="flex items-center flex-row">
+            <span>
+              <Loader className="h-4 w-4 animate-spin mr-2" />
+            </span>
+            {activeStep.description}
+          </div>
+        ),
+        action: (
+          <Button type="button" variant="outline" onClick={() => setOpen(true)}>
+            View Progress
+          </Button>
+        ),
+      });
+    } else if (errorStep) {
+      // Show error toast
+      toast({
+        duration: 5000,
+        variant: "destructive",
+        title: "Failed",
+        description: errorStep.description,
+        action: (
+          <ToastAction altText="View Details" onClick={() => setOpen(true)}>
+            <span>
+              <XCircle className="h-4 w-4 mr-2" />
+            </span>
+            View Details
+          </ToastAction>
+        ),
+      });
+    } else if (isComplete) {
+      // Show success toast that auto-dismisses after 5 seconds
+      toast({
+        duration: 5000,
+        title: "Completed",
+        description: (
+          <div className="flex items-center flex-row">
+            <CheckCircle2 className="h-4 w-4 text-green-600 mr-2" />
+            {lastStep.description}
+          </div>
+        ),
+        action: (
+          <ToastAction altText="View Details" onClick={() => setOpen(true)}>
+            View Details
+          </ToastAction>
+        ),
+      });
+    }
+  }, [dialogSteps, setOpen, toast, open]);
+
+  return null;
+};
+
 export const StepProcessDialogContext = createContext<{
+  open: boolean;
   setDialogStep: (
     step: DialogStep["id"],
     newState?: StepState,
@@ -44,6 +124,7 @@ export const StepProcessDialogContext = createContext<{
   dialogSteps: DialogStep[];
   setExtraContent: React.Dispatch<React.SetStateAction<React.ReactNode>>;
 }>({
+  open: false,
   setDialogStep: async () => Promise.resolve(),
   setSteps: () => {},
   setOpen: () => {},
@@ -126,6 +207,7 @@ export const StepProcessDialogProvider = ({
   return (
     <StepProcessDialogContext.Provider
       value={{
+        open,
         setDialogStep,
         setSteps,
         setOpen,
@@ -198,71 +280,75 @@ const StepProcessModal = ({
   const isLastStepCompleted = lastStep?.state === "completed";
 
   return (
-    <Dialog open={open} onOpenChange={onOpenChange} modal>
-      {triggerLabel && (
-        <DialogTrigger
-          asChild
-          className={buttonVariants({ variant: "secondary" })}
-        >
-          {triggerLabel}
-        </DialogTrigger>
-      )}
-      <DialogContent className="max-w-[500px]">
-        <DialogHeader>
-          <DialogTitle className="font-serif text-3xl font-normal">
-            {title}
-          </DialogTitle>
-        </DialogHeader>
-        <DialogDescription hidden>
-          Shows the status of the transaction
-        </DialogDescription>
-        <div className="flex flex-col px-2 pt-3">
-          {steps.map((step, index) => (
-            <div
-              key={step.id}
-              className="flex items-center relative border-l-2 border-slate-300 pl-2 pb-6 last-of-type:pb-0"
-            >
+    <>
+      <ToastStepper />
+
+      <Dialog open={open} onOpenChange={onOpenChange} modal>
+        {triggerLabel && (
+          <DialogTrigger
+            asChild
+            className={buttonVariants({ variant: "secondary" })}
+          >
+            {triggerLabel}
+          </DialogTrigger>
+        )}
+        <DialogContent className="max-w-[500px]">
+          <DialogHeader>
+            <DialogTitle className="font-serif text-3xl font-normal">
+              {title}
+            </DialogTitle>
+          </DialogHeader>
+          <DialogDescription hidden>
+            Shows the status of the transaction
+          </DialogDescription>
+          <div className="flex flex-col px-2 pt-3">
+            {steps.map((step, index) => (
               <div
-                className={cn(
-                  "p-1 absolute -left-[14px] top-[2px] bg-slate-100 rounded-full",
-                  stateSpecificIconClasses[step.state],
-                  step === lastStep &&
-                    isLastStepCompleted &&
-                    "text-green-600 bg-green-100",
-                )}
+                key={step.id}
+                className="flex items-center relative border-l-2 border-slate-300 pl-2 pb-6 last-of-type:pb-0"
               >
-                {createElement(stepStateIcons[step.state], {
-                  size: 18,
-                })}
-              </div>
-              <div className="flex flex-col pl-4 justify-center">
-                <p
+                <div
                   className={cn(
-                    "text-lg",
-                    stateSpecificTextClasses[step.state],
+                    "p-1 absolute -left-[14px] top-[2px] bg-slate-100 rounded-full",
+                    stateSpecificIconClasses[step.state],
                     step === lastStep &&
                       isLastStepCompleted &&
-                      "text-green-600",
+                      "text-green-600 bg-green-100",
                   )}
                 >
-                  {step.description}
-                </p>
-                {step.state === "error" && step.errorMessage && (
-                  <ScrollArea className="w-96 h-16 rounded p-2 bg-red-50">
-                    <p className="text-red-500 text-xs font-mono">
-                      ({step.errorMessage})
-                    </p>
-                    <ScrollBar orientation="horizontal" />
-                  </ScrollArea>
-                )}
+                  {createElement(stepStateIcons[step.state], {
+                    size: 18,
+                  })}
+                </div>
+                <div className="flex flex-col pl-4 justify-center">
+                  <p
+                    className={cn(
+                      "text-lg",
+                      stateSpecificTextClasses[step.state],
+                      step === lastStep &&
+                        isLastStepCompleted &&
+                        "text-green-600",
+                    )}
+                  >
+                    {step.description}
+                  </p>
+                  {step.state === "error" && step.errorMessage && (
+                    <ScrollArea className="w-96 h-16 rounded p-2 bg-red-50">
+                      <p className="text-red-500 text-xs font-mono">
+                        ({step.errorMessage})
+                      </p>
+                      <ScrollBar orientation="horizontal" />
+                    </ScrollArea>
+                  )}
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
-        <div className="flex flex-col px-2 pt-3">{extraContent}</div>
-      </DialogContent>
-    </Dialog>
+            ))}
+          </div>
+          <div className="flex flex-col px-2 pt-3">{extraContent}</div>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
 
-export { StepProcessModal as default };
+export { StepProcessModal as default, ToastStepper };
